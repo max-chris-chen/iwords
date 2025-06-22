8faae405499443d44e6f1890c8c2689c842206bd
max <max.chen@viljasolutions.com>
2025-06-22 22:23:00 +0800

    chore: Update package.json and pnpm-lock.yaml to remove @sveltejs/adapter-node and switch to @sveltejs/adapter-auto with adjusted configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      outDir: ".svelte-kit/iwords",
+    }),
   },
 };
 
